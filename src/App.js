8c611617547bcf7d3f81d73fcbffd98461301691
Krushna Waghumbare <waghumbarekrushna143@gmail.com>
2024-05-20 00:48:09 +0530

    create about page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import AboutPage from './components/AboutPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ashish bahadure git ashish </h1>
-      <h1>hello pankaj welcome new branch</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-      <h1>krushna is added</h1>
-    </div>
+<>
+<AboutPage/>
+</>
   );
 }
 
