e01d500f32fefa7b3ff006067b75011656ed28a5
prajwal kale <prajwalkale2444@gmail.com>
2024-05-19 23:19:51 +0530

    para deleted

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
     <div className="App">
       <h1>New git Repository</h1>
       <h1>hello pankaj welcome new branch</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
       <h1>krushna is added</h1>
       <h1>hello Prajwal kale</h1>
       <h2>hello amit hi </h2>
