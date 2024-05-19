cd491ba8c5530708c37f3802c7c3fe04d8482f30
prajwal kale <prajwalkale2444@gmail.com>
2024-05-19 23:33:03 +0530

    pankaj deleted

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
   return (
     <div className="App">
       <h1>New git Repository</h1>
-      <h1>hello pankaj welcome new branch</h1>
       <h1>krushna is added</h1>
       <h1>hello Prajwal kale</h1>
       <h2>hello amit hi </h2>
+      <h3>pankaj</h3>
     </div>
   );
 }
