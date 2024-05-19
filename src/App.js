cb043b69e3c198b7112e15b2db4ba0650f2c2085
prajwal kale <prajwalkale2444@gmail.com>
2024-05-19 23:03:48 +0530

    amit added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>ashish bahadure git ashish </h1>
+      <h1>New git Repository</h1>
       <h1>hello pankaj welcome new branch</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
       <h1>krushna is added</h1>
